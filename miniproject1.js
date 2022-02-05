f712f6898d2409cad6c08ffdc3f002e9638935ae
ctyhuofc <98438791+ctyhuofc@users.noreply.github.com>
2022-02-05 10:45:22 -0700

    1st commit

---
+++ miniproject1.js
@@ -0,0 +1,13 @@
+//function calculateGPA(arr){
+//    console.log (arr.length)
+
+
+
+//}
+
+var grades=["a", "b", "c", "d"];
+
+grades.forEach(grade => console.log(grade));
+
+//calculateGPA(grades);
+
